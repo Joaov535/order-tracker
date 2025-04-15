0ee296bb8242be462039ed0fb5cee2d3b3af030e
João Victor Araujo Cruz <94649350+Joaov535@users.noreply.github.com>
2025-04-15 10:39:36 -0300

    nomeando como classe abstrata

--- src/Models/CarriersAbstract.php
+++ src/Models/CarriersAbstract.php
@@ -4,11 +4,13 @@ namespace Joaov535\OrderTracker\Models;
 
 use Joaov535\OrderTracker\Interfaces\CarriersInterface;
 
-abstract class Carriers implements CarriersInterface
+abstract class CarriersAbstract implements CarriersInterface
 {
     protected Order $order;
+    protected Response $response;
     public function __construct(Order $order)
     {
         $this->order = $order;
+        $this->response = new Response();
     }
 }
