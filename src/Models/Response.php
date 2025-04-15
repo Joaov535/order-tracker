3027c4ab122d38a72f30efedc626cf002af86a8a
João Victor Araujo Cruz <94649350+Joaov535@users.noreply.github.com>
2025-04-15 10:40:10 -0300

    add: classe para padronizar retorno

---
+++ src/Models/Response.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Joaov535\OrderTracker\Models;
+
+use DateTime;
+use DateTimeInterface;
+
+class Response
+{
+    protected int $serial;
+    protected string $carrierCode;
+    protected DateTimeInterface $deliveryForecast;
+    protected DateTimeInterface $deliveryDate;
+    protected string $status;
+    protected string $details;
+    protected DateTimeInterface $lastUpdate;
+
+    public function __construct($serial, $carrierCode, $deliveryForecast, $deliveryDate, $status, $details, $lastUpdate)
+    {
+        $this->serial = $serial;
+        $this->carrierCode = $carrierCode;
+        $this->deliveryForecast = new DateTime($deliveryForecast);
+        $this->deliveryDate = new DateTime($deliveryDate);
+        $this->status = $status;
+        $this->details = $details;
+        $this->lastUpdate = new DateTime($lastUpdate);
+    }
+
+    public function getSerial(): int
+    {
+        return $this->serial;
+    }
+
+    public function getCarrierCode(): string
+    {
+        return $this->carrierCode;
+    }
+
+    public function getDeliveryForecast(): DateTimeInterface
+    {
+        return $this->deliveryForecast;
+    }
+
+    public function getDeliveryDate(): DateTimeInterface
+    {
+        return $this->deliveryDate;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function getDetails(): string
+    {
+        return $this->details;
+    }
+
+    public function getLastUpdate(): DateTimeInterface
+    {
+        return $this->lastUpdate;
+    }
+}
