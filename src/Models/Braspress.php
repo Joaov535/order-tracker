2a644f43b3b0827f8a124b8d2e95b6760e0e5824
João Victor Araujo Cruz <94649350+Joaov535@users.noreply.github.com>
2025-04-29 10:14:17 -0300

    verificando erros no parametro

--- src/Models/Braspress.php
+++ src/Models/Braspress.php
@@ -12,40 +12,84 @@ class Braspress extends CarriersAbstract
 
     public function makeRequest(): ?Response
     {
-        $token = $this->order->token ?? base64_encode($this->order->user . ":" . $this->order->pass);
+        try {
+            $token = $this->order->token ?? base64_encode($this->order->user . ":" . $this->order->pass);
 
-        $client = new Client();
-        $res = $client->request(
-            'GET',
-            static::ENDPOINT . $this->order->cnpj . "/" . $this->order->serial . "/json",
-            [
-                "headers" => [
-                    "Authorization" => "Basic " . $token,
-                    'Accept' => 'application/json',
+            $client = new Client();
+            $res = $client->request(
+                'GET',
+                static::ENDPOINT . $this->order->cnpj . "/" . $this->order->serial . "/json",
+                [
+                    "headers" => [
+                        "Authorization" => "Basic " . $token,
+                        'Accept' => 'application/json',
+                    ]
                 ]
-            ]
-        );
-
-        if ($res->getStatusCode() != "200") {
-            return null;
-        }
-
-        $result = json_decode($res->getBody());
-        if (isset($result->conhecimentos[0])) {
-            $data = $result->conhecimentos[0];
-            $this->response = new Response(
-                $this->order->serial,
-                $data->numero ?? null,
-                DateTime::createFromFormat("d/m/Y", $data->previsaoEntrega) ?: null,
-                DateTime::createFromFormat("d/m/Y", $data->dataEntrega) ?: null,
-                $data->status ?? null,
-                $data->ultimaOcorrencia ?? null,
-                DateTime::createFromFormat("d/m/Y H:i:s", $data->dataOcorrencia) ?: null,
             );
 
+            if ($res->getStatusCode() != "200") {
+                return null;
+            }
+
+            $result = json_decode($res->getBody());
+            if (isset($result->conhecimentos[0])) {
+                $data = $result->conhecimentos[0];
+                $this->response = new Response(
+                    $this->order->serial,
+                    $data->numero ?? null,
+                    DateTime::createFromFormat("d/m/Y", $data->previsaoEntrega) ?: null,
+                    DateTime::createFromFormat("d/m/Y", $data->dataEntrega) ?: null,
+                    $data->status ?? null,
+                    $data->ultimaOcorrencia ?? null,
+                    DateTime::createFromFormat("d/m/Y H:i:s", $data->dataOcorrencia) ?: null,
+                );
+            } else if (isset($result->statusCode)) {
+                $this->response = new Response(
+                    $this->order->serial,
+                    null,
+                    null,
+                    null,
+                    null,
+                    $result->message ?: null,
+                    null,
+                    true
+                );
+            } else {
+                $this->response = new Response(
+                    $this->order->serial,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    false
+                );
+            }
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $this->response = new Response(
+                $this->order->serial,
+                null,
+                null,
+                null,
+                "Erro na requisição",
+                $e->getResponse()->getStatusCode(),
+                null,
+                true
+            );
+        }catch(\Exception $e) {
+            $this->response = new Response(
+                $this->order->serial,
+                null,
+                null,
+                null,
+                "Erro na requisição",
+                $e->getCode(),
+                null,
+                true
+            );
+        } finally {
             return $this->response;
         }
-
-        return null;
     }
 }
